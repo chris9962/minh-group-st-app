f2080ae6edd6b3e1e64f3c61a882ed76dd236a02
chuyentina2 <chuyentina2@gmail.com>
2026-07-30 21:14:43 +0700

    Thêm 10 bệnh viện thật vào dữ liệu mẫu danh mục bệnh viện

--- src/mocks/hospitalCatalog.ts
+++ src/mocks/hospitalCatalog.ts
@@ -6,6 +6,16 @@ let hospitals: Hospital[] = [
   { id: 'hv-2', name: 'Bệnh viện Chợ Rẫy' },
   { id: 'hv-3', name: 'Bệnh viện Nhân dân 115' },
   { id: 'hv-4', name: 'Bệnh viện Nhi đồng 1' },
+  { id: 'hv-5', name: 'Quân y Cà Mau' },
+  { id: 'hv-6', name: 'Quân y Bạc Liêu' },
+  { id: 'hv-7', name: 'Quân y Sóc Trăng' },
+  { id: 'hv-8', name: 'Thạnh Trị' },
+  { id: 'hv-9', name: 'Ngã Năm' },
+  { id: 'hv-10', name: 'Phước Long' },
+  { id: 'hv-11', name: 'Châu Thành' },
+  { id: 'hv-12', name: 'Mỹ Tú' },
+  { id: 'hv-13', name: 'Long Mỹ' },
+  { id: 'hv-14', name: 'Hòa Bình' },
 ];
 
 let nextId = 1;
